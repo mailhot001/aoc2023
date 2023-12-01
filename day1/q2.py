0b10af098b3b401910f0f80ffa5da80ac74d8a83
Mailhot <36643298+Mailhot@users.noreply.github.com>
2023-12-01 15:40:18 -0500

    working on 2nd question

--- day1/q2.py
+++ day1/q2.py
@@ -1,13 +1,25 @@
-#6:35 -
+#6:35 - 6:45
+#8:53-
 
 
-
+num_dict = {'one': 1, 
+            'two': 2, 
+            'three': 3,
+            'four': 4,
+            'five': 5,
+            'six': 6,
+            'seven': 7,
+            'eight': 8, 
+            'nine': 9,
+            }
 
 with open('./input.txt') as the_file:
     lines = the_file.readlines()
 
     value_list = []
     for i, line in enumerate(lines):
+        index_min = len(line)-1 
+        index_max = 0
         print()
         print(line)
         print(i)
@@ -15,18 +27,54 @@ with open('./input.txt') as the_file:
             try:
                 int_element = int(element)
                 value1 = int_element
+                index_value1 = index_
                 print(index_, value1)
+                break
             except:
                 continue
+
+        for key in num_dict.keys():
+            string_num1 = line.find(key, 0, index_)
+            if string_num1 == -1:
+                continue
+            elif string_num1 < index_min:
+                index_min = string_num1
+                
+            else: 
+                continue
+
+        if index_min < index_value1:
+            value1_out = index_min
+        elif index_min > index_value1:
+            value1_out = index_value1
+
+        print('value1_out', value1_out)
+
         print('------')
         for index_, element in enumerate(reversed(line)):
             try:
                 int_element = int(element)
                 value2 = int_element
                 print((len(line)-index_-1), value2)
+                index_value2 = index_
+                break
             except:
                 continue
-        current_value = value2 * 10 + value1
+        for key in num_dict.keys():
+            string_num2 = line.find(key, 0, index_)
+            if string_num2 == -1:
+                continue
+            elif string_num2 > index_max:
+                index_max = string_num2 + len(key) #trouve la fin du mot
+            else: 
+                continue
+
+        if index_max > value2:
+            value2_out = index_max
+        elif index_max > value2:
+            value2_out = value2
+
+        current_value = value1_out * 10 + value2_out
         print(current_value)
         value_list.append(current_value)
 
